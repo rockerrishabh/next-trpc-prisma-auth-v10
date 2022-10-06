6178423429db61da0b4b94004a2d293c4b83b91b
Rishabh <rishabh.6542@gmail.com>
2022-10-06 14:24:48 +0530

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,11 @@ const Home: NextPage = () => {
             <button
               className="rounded bg-red-500/90 text-white hover:bg-red-500/80 py-2 px-4"
               type="button"
-              onClick={() => signOut()}
+              onClick={() =>
+                signOut({
+                  callbackUrl: "/",
+                })
+              }
             >
               Sign Out
             </button>
@@ -41,7 +45,11 @@ const Home: NextPage = () => {
           <button
             className="rounded bg-blue-500/90 text-white hover:bg-blue-500/80 py-2 px-4"
             type="button"
-            onClick={() => signIn("google")}
+            onClick={() =>
+              signIn("google", {
+                callbackUrl: "/",
+              })
+            }
           >
             Sign In
           </button>
